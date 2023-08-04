274b435a634e440f99d2b1b0950b0289db6aea3a
Md Shams Firoz <Shams@antino.io>
2023-08-05 00:45:41 +0530

    this first commit to master branch

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('./src/config/express');
+const config = require('./src/config/env/config');
+const connectDB = require('./src/config/database/dbConnection')
+const app = express();
+connectDB();
+const PORT = 9000
+
+// app.get('/', (req, res) => {
+//   res.send('hello world new latest firoz' )
+// })
+app.get('/', (req, res) => {
+  const indexPath = path.join(__dirname, 'index.html');
+  res.sendFile(indexPath);
+});
+
+
+app.listen(process.env.PORT || config.port, function () {
+    console.log('----------------------------------------------------------');
+    console.log(`Server listening at port  http://localhost:${PORT}`);
+    console.log('Time : ' + new Date());
+    console.log('----------------------------------------------------------');
+});
