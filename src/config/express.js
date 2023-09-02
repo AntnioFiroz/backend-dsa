84a1e8694c42850646bc9e5ed7a1cbeb42a25015
Md Shams Firoz <109755992+AntnioFiroz@users.noreply.github.com>
2023-09-02 20:22:53 +0530

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -21,7 +21,7 @@ module.exports = () => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>AngleView - Home</title>
+        <title>Md Shams- Home</title>
         <style>
             /* Inline CSS styles */
             body {
